ff49c874feabd5e231fa6a8214d3e841a8db5b9b
flemingat <103140208+flemingat@users.noreply.github.com>
2023-05-01 13:42:46 -0400

    Update graphGen.rb

--- exact_solution/graphGen.rb
+++ exact_solution/graphGen.rb
@@ -1,22 +1,15 @@
-# Define the number of vertices and edges in the graph
-num_vertices = 10
-num_edges = 16
-
-# Generate a list of all possible edges in the graph
-all_edges = []
-(0...num_vertices).each do |i|
-  (i+1...num_vertices).each do |j|
-    all_edges << [i, j]
+vertexCount = 10
+edgeCount = 16
+edges = []
+(0...vertexCount).each do |i|
+  (i+1...vertexCount).each do |j|
+    edges << [i, j]
   end
 end
-
-# Shuffle the list of edges and select the first num_edges
-random_edges = all_edges.shuffle.take(num_edges)
-
-# Open a file named "genGraph.txt" and write the graph to it
+mixEdge = edges.shuffle.take(edgeCount)
 File.open("genGraph.txt", "w") do |file|
-  file.puts "#{num_edges}"
-  random_edges.each do |edge|
+  file.puts "#{edgeCount}"
+  mixEdge.each do |edge|
     file.puts "#{edge[0]} #{edge[1]}"
   end
 end
